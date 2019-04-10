d3640e70c37e1480d13873b58eb10b5c0534df20
andrewstich <andyroo368@gmail.com>
2019-04-10 16:41:23 -0700

    updated header for phase 7

---
+++ phase7/code/proc.h
@@ -0,0 +1,15 @@
+// proc.h, 159
+
+#ifndef __PROC__		//AS
+#define __PROC__		//AS
+
+// prototype those in proc.c here
+void Delay(void);		//AS
+void ShowChar(int, int, char);	//AS
+void InitProc(void);		//AS
+void Aout(int);
+void UserProc(void);		//AS
+void Ouch(int);
+void Wrapper(int, int)
+
+#endif				//AS
