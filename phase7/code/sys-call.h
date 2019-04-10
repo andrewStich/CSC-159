0d56a56b8d62a772c826614e9a5b487a777532cb
andrewstich <andyroo368@gmail.com>
2019-04-10 16:32:25 -0700

    updated header for phase 7

---
+++ phase7/code/sys-call.h
@@ -0,0 +1,22 @@
+// sys-call.h
+// phase 2
+// CSC 159
+// BDE
+
+#ifndef __SYS_CALL__
+#define __SYS_CALL__
+
+int GetPidCall(void);
+void ShowCharCall(int, int, char);
+void SleepCall(int);
+int MuxCreateCall(int);
+void MuxOpCall(int, int);
+void WriteCall(int, char *);
+void ReadCall(int, char *);
+int ForkCall(void);
+int WaitCall(void);
+void ExitCall(int);
+void ExecCall(int, int);
+void SignalCall(int, int);
+
+#endif
