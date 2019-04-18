1ceeb43f5266d5c8496243a0d2bb57824f43b52e
andrewstich <andyroo368@gmail.com>
2019-04-18 09:44:48 -0700

    added new SR's

---
+++ phase8/code/k-sr.h
@@ -0,0 +1,30 @@
+// k-sr.h, 159
+
+#ifndef __K_SR__
+#define __K_SR__
+
+#include "k-type.h" // for func_p_t
+
+// prototype those in k-sr.c here
+void NewProcSR(func_p_t);	//AS
+void CheckWakeProc(void);
+void TimerSR(void);		//AS
+void SleepSR(int);
+int GetPidSR(void);
+void ShowCharSR(int, int, char);
+int MuxCreateSR(int);
+void MuxOpSR(int, int);
+void TermSR(int);
+void TermRxSR(int);
+void TermTxSR(int);
+int ForkSR(void);
+int WaitSR(void);
+void ExitSR(int);
+void ExecSR(int, int);
+void SignalSR(int, int);
+void WrapperSR(int, int, int);
+void PauseSR(void);
+void KillSR(int, int);
+unsigned RandSR(void);
+
+#endif
